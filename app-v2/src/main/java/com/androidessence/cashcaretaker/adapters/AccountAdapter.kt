60bcbd22857f5d94377b486e90306f607ac02161
AdamMc331 <amcneilly331@gmail.com>
2017-03-19 13:41:54 -0400

    Added account adapter.

---
+++ app-v2/src/main/java/com/androidessence/cashcaretaker/adapters/AccountAdapter.kt
@@ -0,0 +1,35 @@
+package com.androidessence.cashcaretaker.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.adammcneilly.CoreRecyclerViewAdapter
+import com.adammcneilly.CoreViewHolder
+import com.androidessence.cashcaretaker.R
+import com.androidessence.cashcaretaker.models.Account
+import com.androidessence.utility.asCurrency
+
+/**
+ * Adapter that displays a list of account object.
+ *
+ * Created by adam.mcneilly on 3/19/17.
+ */
+open class AccountAdapter : CoreRecyclerViewAdapter<Account, AccountAdapter.AccountViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): AccountViewHolder {
+        val context = parent?.context
+        val view = LayoutInflater.from(context).inflate(R.layout.list_item_account, parent, false)
+        return AccountViewHolder(view)
+    }
+
+    open class AccountViewHolder(view: View?) : CoreViewHolder<Account>(view) {
+        private val accountName: TextView? = view?.findViewById(R.id.account_name) as? TextView
+        private val accountBalance: TextView? = view?.findViewById(R.id.account_balance) as? TextView
+
+        override fun bindItem(item: Account?) {
+            accountName?.text = item?.name
+            accountBalance?.text = item?.balance?.asCurrency()
+        }
+    }
+}
