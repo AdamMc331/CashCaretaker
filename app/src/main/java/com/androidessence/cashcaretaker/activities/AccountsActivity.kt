bf856c7aea0383e5e8cb5dc9d61ae5cfa6926225
AdamMc331 <amcneilly331@gmail.com>
2017-06-11 20:38:13 -0400

    Closes #48. Removed Wear code.

--- app/src/main/java/com/androidessence/cashcaretaker/activities/AccountsActivity.kt
+++ app/src/main/java/com/androidessence/cashcaretaker/activities/AccountsActivity.kt
@@ -6,32 +6,19 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.androidessence.cashcaretaker.DatabaseToJSON
 import com.androidessence.cashcaretaker.R
-import com.androidessence.cashcaretaker.adapters.AccountAdapter
 import com.androidessence.cashcaretaker.alarms.RepeatingTransactionAlarm
 import com.androidessence.cashcaretaker.core.CoreActivity
 import com.androidessence.cashcaretaker.fragments.AccountsFragment
-import com.androidessence.utility.default
 import com.crashlytics.android.Crashlytics
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.wearable.Wearable
 import io.fabric.sdk.android.Fabric
-import org.json.JSONException
-import timber.log.Timber
 
 /**
  * Activity that displays a list of accounts to the user.
 
  * Created by adam.mcneilly on 9/5/16.
  */
-class AccountsActivity : CoreActivity(),
-        GoogleApiClient.OnConnectionFailedListener,
-        AccountAdapter.OnAccountDeletedListener,
-        GoogleApiClient.ConnectionCallbacks {
-
-    private var googleApiClient: GoogleApiClient? = null
+class AccountsActivity : CoreActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +27,6 @@ class AccountsActivity : CoreActivity(),
 
         setupToolbar(false)
         startAlarm()
-        setupClient()
 
         val accountsFragment = AccountsFragment.newInstance()
         supportFragmentManager.beginTransaction().replace(R.id.fragment_accounts, accountsFragment, "TODO:").commit()
@@ -81,62 +67,5 @@ class AccountsActivity : CoreActivity(),
 
         val alarm = RepeatingTransactionAlarm()
         alarm.setAlarm(this)
-    }
-
-    private fun setupClient() {
-        googleApiClient = GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build()
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        // Connect to data layer
-        googleApiClient?.connect()
-    }
-
-    override fun onConnected(bundle: Bundle?) {
-        // Send over accounts
-        try {
-            val accountsJson = DatabaseToJSON(this).accountJSON.toString()
-            SendToDataLayerThread(getString(R.string.add_account_path), accountsJson).start()
-        } catch (joe: JSONException) {
-            Timber.e(joe)
-        }
-
-    }
-
-    override fun onStop() {
-        // Disconnect
-        if (googleApiClient?.isConnected.default(false)) googleApiClient?.disconnect()
-
-        super.onStop()
-    }
-
-    override fun onConnectionSuspended(i: Int) {
-
-    }
-
-    override fun onConnectionFailed(connectionResult: ConnectionResult) {
-
-    }
-
-    private inner class SendToDataLayerThread(private val mPath: String, private val mMessage: String) : Thread() {
-
-        override fun run() {
-            val nodes = Wearable.NodeApi.getConnectedNodes(googleApiClient).await()
-
-            nodes.nodes.forEach {
-                Wearable.MessageApi.sendMessage(googleApiClient, it.id, mPath, mMessage.toByteArray()).await()
-            }
-        }
-    }
-
-    override fun onAccountDeleted(id: Long) {
-        // Send id as string
-        SendToDataLayerThread(getString(R.string.delete_account_path), id.toString()).start()
     }
 }
