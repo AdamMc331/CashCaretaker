3dbc92392ffdd063401901a4b5312f79005e79fc
AdamMc331 <amcneilly331@gmail.com>
2017-11-21 00:18:44 -0500

    Closes #56. Removed Utility.kt and modified tests. Also setup Travis to run unit tests on each push.

--- utility/src/main/java/com/androidessence/utility/DateUtils.kt
+++ utility/src/main/java/com/androidessence/utility/DateUtils.kt
@@ -1,30 +1,37 @@
 package com.androidessence.utility
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
  * Utility methods for Dates in Kotlin
- *
- * Created by adam.mcneilly on 2/6/17.
  */
+
+object DateFormats {
+    private val UI_DATE_FORMAT = "MMMM dd, yyyy"
+    val UI_DATE_FORMATTER = SimpleDateFormat(UI_DATE_FORMAT, Locale.getDefault())
+}
+
 fun Date.asCalendar(): Calendar {
     val c = Calendar.getInstance()
     c.time = this
     return c
 }
 
-fun Date.get(field: Int): Int {
-    return this.asCalendar().get(field)
-}
+fun Date.get(field: Int): Int = this.asCalendar().get(field)
 
-fun Date.year(): Int {
-    return this.get(Calendar.YEAR)
-}
+fun Date.year(): Int = this.get(Calendar.YEAR)
 
-fun Date.month(): Int {
-    return this.get(Calendar.MONTH)
-}
+fun Date.month(): Int = this.get(Calendar.MONTH)
 
-fun Date.day(): Int {
-    return this.get(Calendar.DAY_OF_MONTH)
+fun Date.day(): Int = this.get(Calendar.DAY_OF_MONTH)
+
+fun Date?.asUIString(): String {
+    val year = this?.year() ?: -1
+
+    if (year < 0) {
+        throw UnsupportedOperationException("Date has negative year value.")
+    } else {
+        return DateFormats.UI_DATE_FORMATTER.format(this)
+    }
 }
