2b41ba0c801a8eeedbb83202196c90f82aa17fe2
AdamMc331 <amcneilly331@gmail.com>
2017-11-28 20:44:28 -0500

    Closes #80. Will just document more as I touch files.

--- app-refresh/src/main/java/com/androidessence/cashcaretaker/addtransaction/AddTransactionController.kt
+++ app-refresh/src/main/java/com/androidessence/cashcaretaker/addtransaction/AddTransactionController.kt
@@ -5,11 +5,26 @@ import com.androidessence.cashcaretaker.core.DataController
 
 
 /**
- * View to add a transaction.
+ * Controller for adding a transaction.
  */
 interface AddTransactionController : DataController, DatePickerDialog.OnDateSetListener {
+    /**
+     * Callback when the transaction description supplied is invalid.
+     */
     fun showTransactionDescriptionError()
+
+    /**
+     * Callback when the transaction amount supplied is invalid.
+     */
     fun showTransactionAmountError()
+
+    /**
+     * Callback with the identifiers of the inserted accounts.
+     */
     fun onInserted(ids: List<Long>)
+
+    /**
+     * Displays a date picker that allows the user to pick the date of their transaction.
+     */
     fun showDatePicker()
 }
--- app-refresh/src/main/java/com/androidessence/cashcaretaker/addtransaction/AddTransactionPresenter.kt
+++ app-refresh/src/main/java/com/androidessence/cashcaretaker/addtransaction/AddTransactionPresenter.kt
@@ -7,9 +7,30 @@ import java.util.*
  * Presenter for adding an account.
  */
 interface AddTransactionPresenter: BasePresenter {
+
+    /**
+     * Callback used when a list of transactions are inserted.
+     */
     fun onInserted(ids: List<Long>)
+
+    /**
+     * Callback used if the transaction description is invalid.
+     */
     fun onTransactionDescriptionError()
+
+    /**
+     * Callback used if the transaction amount is invalid.
+     */
     fun onTransactionAmountError()
 
+    /**
+     * Validates and inserts a transaction into the database.
+     *
+     * @param[accountName] The name of the account for this transaction.
+     * @param[transactionDescription] The description of the purchase.
+     * @param[transactionAmount] The amount of the transaction.
+     * @param[withdrawal] Flag defining whether this transaction is a withdrawal.
+     * @param[date] The date of the purchase.
+     */
     fun insert(accountName: String, transactionDescription: String, transactionAmount: String, withdrawal: Boolean, date: Date)
 }
