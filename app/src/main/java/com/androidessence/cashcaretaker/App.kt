84159ca9f8e9c6f579477a6d00a8e4808aaf43ea
Adam McNeilly <amcneilly331@gmail.com>
2020-06-27 15:41:33 -0400

    Adding some DI framework. (#195)
    * Adding some DI framework.

--- app/src/main/java/com/androidessence/cashcaretaker/App.kt
+++ app/src/main/java/com/androidessence/cashcaretaker/App.kt
@@ -1,12 +1,20 @@
 package com.androidessence.cashcaretaker
 
 import android.app.Application
+import android.content.Context
+import com.androidessence.cashcaretaker.di.BaseCashCaretakerGraph
+import com.androidessence.cashcaretaker.di.CashCaretakerGraph
+import com.androidessence.cashcaretaker.di.CashCaretakerGraphProvider
 import timber.log.Timber
 
 /**
  * Core application class that keeps a context and initializes Timber.
  */
-class App : Application() {
+class App : Application(), CashCaretakerGraphProvider {
+
+    override val graph: CashCaretakerGraph by lazy {
+        BaseCashCaretakerGraph(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -15,4 +23,8 @@ class App : Application() {
             Timber.plant(Timber.DebugTree())
         }
     }
+}
+
+fun Context.graph(): CashCaretakerGraph {
+    return (this.applicationContext as CashCaretakerGraphProvider).graph
 }
