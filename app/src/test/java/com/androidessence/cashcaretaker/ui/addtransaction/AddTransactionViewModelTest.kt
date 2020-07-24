af01586b05890c5323e74b69903235b6727de560
adammc331 <amcneilly331@gmail.com>
2020-07-24 17:11:33 -0400

    Adding test for invalid transaction errors.

--- app/src/test/java/com/androidessence/cashcaretaker/ui/addtransaction/AddTransactionViewModelTest.kt
+++ app/src/test/java/com/androidessence/cashcaretaker/ui/addtransaction/AddTransactionViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.androidessence.cashcaretaker.ui.addtransaction
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.androidessence.cashcaretaker.R
 import java.util.Date
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
@@ -52,5 +53,40 @@ class AddTransactionViewModelTest {
             .assertCallToUpdateTransaction()
             .assertCallToTrackTransactionEdited()
             .assertDismissEventEmitted()
+    }
+
+    @Test
+    fun addTransactionWithInvalidAmountDisplaysError() {
+        testRobot
+            .buildViewModel()
+            .addTransaction(
+                accountName = "Checking",
+                transactionDescription = "Test transaction",
+                transactionAmount = "This is not currency",
+                withdrawal = true,
+                date = Date()
+            )
+            .assertTransactionAmountError(
+                expectedValue = R.string.error_invalid_amount
+            )
+    }
+
+    @Test
+    fun editTransactionWithInvalidAmountDisplaysError() {
+        val input = AddTransactionViewModel.TransactionInput(
+            id = 0L,
+            accountName = "Checking",
+            transactionDescription = "Test transaction",
+            transactionAmount = "This is not currency",
+            withdrawal = true,
+            date = Date()
+        )
+
+        testRobot
+            .buildViewModel()
+            .updateTransaction(input)
+            .assertTransactionAmountError(
+                expectedValue = R.string.error_invalid_amount
+            )
     }
 }
