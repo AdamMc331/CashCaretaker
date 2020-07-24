e450a71638c1da82cbcf05cdcb04535c27f6ff55
Adam McNeilly <amcneilly331@gmail.com>
2020-07-24 17:51:10 -0400

    Adding tests for AddTransactionViewModel. (#212)
    * Adding tests for AddTransactionViewModel.

---
+++ app/src/test/java/com/androidessence/cashcaretaker/ui/addtransaction/AddTransactionViewModelTest.kt
@@ -0,0 +1,92 @@
+package com.androidessence.cashcaretaker.ui.addtransaction
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.androidessence.cashcaretaker.R
+import java.util.Date
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class AddTransactionViewModelTest {
+    private lateinit var testRobot: AddTransactionViewModelRobot
+
+    @JvmField
+    @Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        testRobot = AddTransactionViewModelRobot()
+    }
+
+    @Test
+    fun addValidTransactionInsertsTracksAndDismisses() = runBlockingTest {
+        testRobot
+            .buildViewModel()
+            .addTransaction(
+                accountName = "Checking",
+                transactionDescription = "Test transaction",
+                transactionAmount = "100.00",
+                withdrawal = true,
+                date = Date()
+            )
+            .assertCallToInsertTransaction()
+            .assertCallToTrackTransactionInserted()
+            .assertDismissEventEmitted()
+    }
+
+    @Test
+    fun editValidTransactionUpdatesTracksAndDismisses() = runBlockingTest {
+        val input = AddTransactionViewModel.TransactionInput(
+            id = 0L,
+            accountName = "Checking",
+            transactionDescription = "Test transaction",
+            transactionAmount = "100.00",
+            withdrawal = true,
+            date = Date()
+        )
+
+        testRobot
+            .buildViewModel()
+            .updateTransaction(input)
+            .assertCallToUpdateTransaction()
+            .assertCallToTrackTransactionEdited()
+            .assertDismissEventEmitted()
+    }
+
+    @Test
+    fun addTransactionWithInvalidAmountDisplaysError() {
+        testRobot
+            .buildViewModel()
+            .addTransaction(
+                accountName = "Checking",
+                transactionDescription = "Test transaction",
+                transactionAmount = "This is not currency",
+                withdrawal = true,
+                date = Date()
+            )
+            .assertTransactionAmountError(
+                expectedValue = R.string.error_invalid_amount
+            )
+    }
+
+    @Test
+    fun editTransactionWithInvalidAmountDisplaysError() {
+        val input = AddTransactionViewModel.TransactionInput(
+            id = 0L,
+            accountName = "Checking",
+            transactionDescription = "Test transaction",
+            transactionAmount = "This is not currency",
+            withdrawal = true,
+            date = Date()
+        )
+
+        testRobot
+            .buildViewModel()
+            .updateTransaction(input)
+            .assertTransactionAmountError(
+                expectedValue = R.string.error_invalid_amount
+            )
+    }
+}
