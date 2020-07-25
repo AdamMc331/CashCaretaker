2ad9824d81a8188f1fa761e8ea1dcf8d436d187d
adammc331 <amcneilly331@gmail.com>
2020-07-25 17:52:13 -0400

    Fixing imports.

--- app/src/main/java/com/androidessence/cashcaretaker/App.kt
+++ app/src/main/java/com/androidessence/cashcaretaker/App.kt
@@ -2,7 +2,11 @@ package com.androidessence.cashcaretaker
 
 import android.app.Application
 import android.content.Context
-import com.androidessence.cashcaretaker.core.di.*
+import com.androidessence.cashcaretaker.core.di.BaseCashCaretakerGraph
+import com.androidessence.cashcaretaker.core.di.CashCaretakerGraph
+import com.androidessence.cashcaretaker.core.di.CashCaretakerGraphProvider
+import com.androidessence.cashcaretaker.core.di.dataModule
+import com.androidessence.cashcaretaker.core.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
