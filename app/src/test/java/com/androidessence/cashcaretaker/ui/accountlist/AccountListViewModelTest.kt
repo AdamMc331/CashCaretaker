7524b0548b1519ea4d5b84bf659ab4096641bfa3
adammc331 <amcneilly331@gmail.com>
2020-07-24 21:38:32 -0400

    Adding tests for AccountListViewModel.

---
+++ app/src/test/java/com/androidessence/cashcaretaker/ui/accountlist/AccountListViewModelTest.kt
@@ -0,0 +1,47 @@
+package com.androidessence.cashcaretaker.ui.accountlist
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.androidessence.cashcaretaker.core.models.Account
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class AccountListViewModelTest {
+    private lateinit var testRobot: AccountListViewModelRobot
+
+    @JvmField
+    @Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        testRobot = AccountListViewModelRobot()
+    }
+
+    @Test
+    fun fetchValidAccounts() {
+        val accounts = listOf(
+            Account(name = "Test Account")
+        )
+
+        testRobot
+            .mockAccounts(accounts)
+            .buildViewModel()
+            .assertAccounts(accounts)
+            .assertShowingAccounts(true)
+            .assertShowLoading(false)
+            .assertShowingEmptyMessage(false)
+    }
+
+    @Test
+    fun fetchEmptyAccounts() {
+        testRobot
+            .mockAccounts(emptyList())
+            .buildViewModel()
+            .assertAccounts(emptyList())
+            .assertShowingAccounts(false)
+            .assertShowLoading(false)
+            .assertShowingEmptyMessage(true)
+    }
+}
