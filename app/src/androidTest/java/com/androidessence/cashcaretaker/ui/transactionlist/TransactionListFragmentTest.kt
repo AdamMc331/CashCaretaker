e1e183db6859f4026c7c1b506eb667f1a8605179
Adam McNeilly <amcneilly331@gmail.com>
2020-08-03 22:09:40 -0400

    Making RoomDatabase internal to keep encapsulation. (#224)

--- app/src/androidTest/java/com/androidessence/cashcaretaker/ui/transactionlist/TransactionListFragmentTest.kt
+++ app/src/androidTest/java/com/androidessence/cashcaretaker/ui/transactionlist/TransactionListFragmentTest.kt
@@ -4,7 +4,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.androidessence.cashcaretaker.core.models.Account
 import com.androidessence.cashcaretaker.database.CCDatabase
-import com.androidessence.cashcaretaker.database.RoomDatabase
 import com.androidessence.cashcaretaker.ui.main.MainActivity
 import com.androidessence.cashcaretaker.ui.main.MainController
 import kotlinx.coroutines.runBlocking
@@ -14,16 +13,17 @@ import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.KoinComponent
+import org.koin.core.get
 
 @RunWith(AndroidJUnit4::class)
-class TransactionListFragmentTest {
+class TransactionListFragmentTest : KoinComponent {
 
     @JvmField
     @Rule
     var activityTestRule = ActivityTestRule(MainActivity::class.java)
 
-    private val database: CCDatabase
-        get() = RoomDatabase(activityTestRule.activity)
+    private val database: CCDatabase = get()
 
     @Before
     fun setUp() {
