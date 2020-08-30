0ccfaaaa53cede450a56f536c69adc7e8bd49942
adammc331 <amcneilly331@gmail.com>
2020-08-30 18:42:33 -0400

    Breaking up responsibilities of AccountListScreenComposable.

--- app/src/main/java/com/androidessence/cashcaretaker/ui/accountlist/AccountListScreenComposable.kt
+++ app/src/main/java/com/androidessence/cashcaretaker/ui/accountlist/AccountListScreenComposable.kt
@@ -38,7 +38,7 @@ fun AccountListScreenLiveData(
 }
 
 @Composable
-fun AccountListScreen(
+private fun AccountListScreen(
     viewState: AccountListState,
     accountClickListener: (Account) -> Unit,
     accountLongClickListener: (Account) -> Unit,
@@ -47,33 +47,55 @@ fun AccountListScreen(
 ) {
     when {
         viewState.loading -> {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                gravity = ContentGravity.Center,
-                backgroundColor = Color.White
-            ) {
-                CircularProgressIndicator()
-            }
+            LoadingState()
         }
         viewState.data.isNotEmpty() -> {
-            Surface(color = Color.White) {
-                ScrollableColumn {
-                    viewState.data.forEach { account ->
-                        AccountListItem(
-                            account = account,
-                            accountClickListener = accountClickListener,
-                            accountLongClickListener = accountLongClickListener,
-                            withdrawalClickListener = withdrawalClickListener,
-                            depositClickListener = depositClickListener
-                        )
-                        Divider(color = Color.LightGray)
-                    }
-                }
-            }
+            AccountListView(
+                viewState,
+                accountClickListener,
+                accountLongClickListener,
+                withdrawalClickListener,
+                depositClickListener
+            )
         }
         else -> {
             NoAccountsMessage()
         }
+    }
+}
+
+@Composable
+private fun AccountListView(
+    viewState: AccountListState,
+    accountClickListener: (Account) -> Unit,
+    accountLongClickListener: (Account) -> Unit,
+    withdrawalClickListener: (Account) -> Unit,
+    depositClickListener: (Account) -> Unit
+) {
+    Surface(color = Color.White) {
+        ScrollableColumn {
+            viewState.data.forEach { account ->
+                AccountListItem(
+                    account = account,
+                    accountClickListener = accountClickListener,
+                    accountLongClickListener = accountLongClickListener,
+                    withdrawalClickListener = withdrawalClickListener,
+                    depositClickListener = depositClickListener
+                )
+                Divider(color = Color.LightGray)
+            }
+        }
+    }
+}
+
+@Composable
+private fun LoadingState() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        gravity = ContentGravity.Center,
+        backgroundColor = Color.White
+    ) {
+        CircularProgressIndicator()
     }
 }
 
