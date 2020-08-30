e4ca7df3c9fd69f5b6242dd767d086a336ee650a
adammc331 <amcneilly331@gmail.com>
2020-08-30 16:34:01 -0400

    Implementing AccountListScreenComposable.

--- app/src/main/java/com/androidessence/cashcaretaker/ui/accountlist/NoAccountsMessage.kt
+++ app/src/main/java/com/androidessence/cashcaretaker/ui/accountlist/NoAccountsMessage.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,26 +24,28 @@ import com.androidessence.cashcaretaker.R
 
 @Composable
 fun NoAccountsMessage() {
-    Column(
-        modifier = Modifier
-            .padding(32.dp)
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Image(
+    Surface(color = Color.White) {
+        Column(
             modifier = Modifier
-                .fillMaxSize(fraction = 0.5F)
-                .gravity(Alignment.CenterHorizontally),
-            asset = vectorResource(id = R.drawable.ic_money_off_24dp),
-            colorFilter = ColorFilter.tint(Color.Black),
-            contentScale = ContentScale.FillWidth
+                .padding(32.dp)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxSize(fraction = 0.5F)
+                    .gravity(Alignment.CenterHorizontally),
+                asset = vectorResource(id = R.drawable.ic_money_off_24dp),
+                colorFilter = ColorFilter.tint(Color.Black),
+                contentScale = ContentScale.FillWidth
 
-        )
-        Text(
-            text = stringResource(id = R.string.no_accounts_message),
-            style = MaterialTheme.typography.body1,
-            textAlign = TextAlign.Center
-        )
+            )
+            Text(
+                text = stringResource(id = R.string.no_accounts_message),
+                style = MaterialTheme.typography.body1,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
 
